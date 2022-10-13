e06f201942a072882876882843bbc3b2f5bc6b61
shahid ahmad <shahid.bangash@hotmail.com>
2022-10-13 14:39:57 +0500

    set params

--- src/app/core/services/flight-destinations.service.ts
+++ src/app/core/services/flight-destinations.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import FlightDestinationsAPI from '@core/api-models/flight-destinations.api-model';
 import FlightDesginationParam from '@core/models/flight-destination-param.model';
@@ -9,10 +9,15 @@ import { Observable, delay, map } from 'rxjs';
 export class FlightDestinationsService {
   constructor(private http: HttpClient) {}
 
-  load(param: FlightDesginationParam): Observable<FlightDestinationsAPI> {
-    return this.http.get('assets/mocks/flight-destinations.json').pipe(
-      delay(2000),
-      map((resp) => ({ ...resp, data: [] }))
-    ) as Observable<FlightDestinationsAPI>;
+  load(queryParam: FlightDesginationParam): Observable<FlightDestinationsAPI> {
+    const params = new HttpParams({
+      fromObject: { ...queryParam },
+    });
+    return this.http
+      .get('assets/mocks/flight-destinations.json', { params })
+      .pipe(
+        delay(2000),
+        map((resp) => ({ ...resp }))
+      ) as Observable<FlightDestinationsAPI>;
   }
 }
